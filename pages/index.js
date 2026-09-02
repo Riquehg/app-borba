e78a1454f725c335788a4ecdbf7045ea54e308c8
Riquehg <rick.2.x.hg@gmail.com>
2026-09-02 17:43:56 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { supabase } from '../lib/supabase';
 
 const defaultTenant = {
   name: "Borba Cordeiros",
-  whatsapp: "5547999999999",
+  whatsapp: "5547996302864",
   primary_color: "#FF8C00",
   logo_url: "https://storage.googleapis.com/prod-cardapio-web/uploads/company/logo/34208/8587d992WhatsApp_Image_2026-06-05_at_10.32.49.jpeg",
   banner_url: "https://storage.googleapis.com/prod-cardapio-web/uploads/company/image/34208/54f8b26fWhatsApp_Image_2026-06-05_at_10.27.33.jpeg"
